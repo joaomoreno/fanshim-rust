6deba213d8e6622dc9e0803e98aea2c786b77ec2
Joao Moreno <mail@joaomoreno.com>
2019-10-26 22:02:11 +0200

    blink

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate sysfs_gpio;
 use std::error::Error;
 use std::thread::sleep;
 use std::time::Duration;
-use sysfs_gpio::Pin;
+use sysfs_gpio::{Direction, Pin};
 
 const DAT: u64 = 15;
 const CLK: u64 = 14;
@@ -20,24 +20,24 @@ struct Pixel {
 fn sof(dat: &Pin, clk: &Pin) -> Result<(), Box<dyn Error>> {
 	dat.set_value(0)?;
 
-	for _ in 0..36 {
+	for _ in 0..32 {
 		clk.set_value(1)?;
-		sleep(Duration::from_nanos(500));
+		sleep(Duration::from_nanos(250));
 		clk.set_value(0)?;
-		sleep(Duration::from_nanos(500));
+		sleep(Duration::from_nanos(250));
 	}
 
 	Ok(())
 }
 
 fn eof(dat: &Pin, clk: &Pin) -> Result<(), Box<dyn Error>> {
-	dat.set_value(0)?;
+	dat.set_value(1)?;
 
 	for _ in 0..32 {
 		clk.set_value(1)?;
-		sleep(Duration::from_nanos(500));
+		sleep(Duration::from_nanos(250));
 		clk.set_value(0)?;
-		sleep(Duration::from_nanos(500));
+		sleep(Duration::from_nanos(250));
 	}
 
 	Ok(())
@@ -47,10 +47,10 @@ fn write_byte(dat: &Pin, clk: &Pin, mut byte: u8) -> Result<(), Box<dyn Error>> 
 	for _ in 0..8 {
 		dat.set_value(byte & 0b10000000)?;
 		clk.set_value(1)?;
-		sleep(Duration::from_nanos(500));
+		sleep(Duration::from_nanos(250));
 		byte <<= 1;
 		clk.set_value(0)?;
-		sleep(Duration::from_nanos(500));
+		sleep(Duration::from_nanos(250));
 	}
 
 	Ok(())
@@ -61,7 +61,7 @@ fn show_pixel(dat: &Pin, clk: &Pin, pixel: &Pixel) -> Result<(), Box<dyn Error>>
 	write_byte(
 		dat,
 		clk,
-		0b11100000 | (((3.0 * pixel.brightness) as u8) & 0b11111),
+		0b1110_0000 | (((31.0 * pixel.brightness) as u8) & 0b1_1111),
 	)?;
 	write_byte(dat, clk, pixel.blue)?;
 	write_byte(dat, clk, pixel.green)?;
@@ -72,18 +72,22 @@ fn show_pixel(dat: &Pin, clk: &Pin, pixel: &Pixel) -> Result<(), Box<dyn Error>>
 
 fn main() -> Result<(), Box<dyn Error>> {
 	let dat = Pin::new(DAT);
-	let clk = Pin::new(CLK);
+	dat.set_direction(Direction::Low)?;
 	dat.export()?;
+	let clk = Pin::new(CLK);
+	clk.set_direction(Direction::Low)?;
 	clk.export()?;
 
-	let pixel = Pixel {
-		red: 255,
-		green: 0,
-		blue: 0,
-		brightness: 0.2,
-	};
-
-	show_pixel(&dat, &clk, &pixel)?;
+	for i in 0i32..100 {
+		let pixel = Pixel {
+			red: 255,
+			green: 0,
+			blue: 0,
+			brightness: 0.1 * (10 - ((i % 20) - 10).abs()) as f32,
+		};
+		show_pixel(&dat, &clk, &pixel)?;
+		sleep(Duration::from_millis(5));
+	}
 
 	Ok(())
 }
