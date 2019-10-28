52b39722c4bf7ca9e6c25baf505e53311809ee5d
Joao Moreno <mail@joaomoreno.com>
2019-10-28 22:20:44 +0100

    wip: button

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate sysfs_gpio;
 use std::error::Error;
-use std::thread::sleep;
+use std::sync::mpsc;
+use std::thread::{sleep, spawn};
 use std::time::Duration;
 use sysfs_gpio::{Direction, Pin};
 
@@ -8,6 +9,13 @@ pub struct FanSHIM {
 	fan: Pin,
 	data: Pin,
 	clock: Pin,
+	button: Pin,
+	on_press: Option<Box<dyn Fn() + Send>>,
+	on_release: Option<Box<dyn Fn() + Send>>,
+	on_hold: Option<Box<dyn Fn() + Send>>,
+	hold_time: f32,
+	polling_handle: Option<mpsc::Sender<()>>,
+	polling_delay: Duration,
 }
 
 impl FanSHIM {
@@ -24,7 +32,22 @@ impl FanSHIM {
 		clock.set_direction(Direction::Low)?;
 		clock.export()?;
 
-		Ok(FanSHIM { fan, data, clock })
+		let button = Pin::new(17);
+		button.set_direction(Direction::Out)?;
+		button.export()?;
+
+		Ok(FanSHIM {
+			fan,
+			data,
+			clock,
+			button,
+			on_press: None,
+			on_release: None,
+			on_hold: None,
+			hold_time: 2.0,
+			polling_handle: None,
+			polling_delay: Duration::from_millis(50),
+		})
 	}
 
 	// https://cdn-shop.adafruit.com/datasheets/APA102.pdf
@@ -63,6 +86,89 @@ impl FanSHIM {
 		self.fan.set_value(on as u8)?;
 		Ok(())
 	}
+
+	pub fn set_on_press<CB: 'static + Fn() + Send>(
+		&mut self,
+		callback: CB,
+	) -> Result<(), Box<dyn Error>> {
+		self.on_press = Some(Box::new(callback));
+		self.on_did_change_callbacks()
+	}
+
+	pub fn unset_on_press<CB: 'static + Fn() + Send>(&mut self) -> Result<(), Box<dyn Error>> {
+		self.on_press = None;
+		self.on_did_change_callbacks()
+	}
+
+	pub fn set_on_release<CB: 'static + Fn() + Send>(
+		&mut self,
+		callback: CB,
+	) -> Result<(), Box<dyn Error>> {
+		self.on_release = Some(Box::new(callback));
+		self.on_did_change_callbacks()
+	}
+
+	pub fn unset_on_release<CB: 'static + Fn() + Send>(&mut self) -> Result<(), Box<dyn Error>> {
+		self.on_release = None;
+		self.on_did_change_callbacks()
+	}
+
+	pub fn set_on_hold<CB: 'static + Fn() + Send>(
+		&mut self,
+		callback: CB,
+	) -> Result<(), Box<dyn Error>> {
+		self.on_hold = Some(Box::new(callback));
+		self.on_did_change_callbacks()
+	}
+
+	pub fn unset_on_hold<CB: 'static + Fn() + Send>(&mut self) -> Result<(), Box<dyn Error>> {
+		self.on_hold = None;
+		self.on_did_change_callbacks()
+	}
+
+	fn on_did_change_callbacks(&mut self) -> Result<(), Box<dyn Error>> {
+		if let (None, None, None) = (
+			self.on_press.as_ref(),
+			self.on_release.as_ref(),
+			self.on_hold.as_ref(),
+		) {
+			if let Some(tx) = &self.polling_handle {
+				tx.send(())?;
+			}
+		} else {
+			if let None = self.polling_handle {
+				let (tx, rx) = mpsc::channel();
+				let delay = self.polling_delay;
+				let button = self.button;
+				// let on_press = self.on_press;
+				// let on_release = self.on_release;
+				// let on_hold = self.on_hold;
+
+				spawn(move || loop {
+					let last = 1u8;
+
+					loop {
+						if let Ok(_) = rx.try_recv() {
+							return;
+						}
+
+						let current = button.get_value().unwrap();
+
+						if last > current {
+							// if let Some(a) = self.on_press {}
+						}
+
+						sleep(delay);
+					}
+				});
+				self.polling_handle = Some(tx);
+			}
+		}
+
+		Ok(())
+	}
+
+	fn start_polling(&self) {}
 
 	fn write_byte(&self, mut byte: u8) -> Result<(), Box<dyn Error>> {
 		for _ in 0..8 {
