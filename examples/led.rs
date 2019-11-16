3ed17c7c0105b674ca984ed19ed1d56a113e0ae9
Joao Moreno <mail@joaomoreno.com>
2019-11-16 17:53:29 +0100

    led

--- examples/led.rs
+++ examples/led.rs
@@ -1,28 +1,39 @@
+extern crate colorsys;
 extern crate fanshim;
 
+use colorsys::{Hsl, Rgb};
 use fanshim::FanSHIM;
 use std::error::Error;
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 fn main() -> Result<(), Box<dyn Error>> {
   let fanshim = FanSHIM::new()?;
-  fanshim.set_led(255, 0, 0, 1.0)?;
-  thread::sleep(Duration::from_millis(250));
-  fanshim.set_led(0, 255, 0, 1.0)?;
-  thread::sleep(Duration::from_millis(250));
-  fanshim.set_led(0, 0, 255, 1.0)?;
-  thread::sleep(Duration::from_millis(250));
-  fanshim.set_led(255, 255, 255, 1.0)?;
-  thread::sleep(Duration::from_millis(250));
-  fanshim.set_led(255, 0, 0, 1.0)?;
-  thread::sleep(Duration::from_millis(250));
-  fanshim.set_led(255, 0, 0, 0.75)?;
-  thread::sleep(Duration::from_millis(250));
-  fanshim.set_led(255, 0, 0, 0.5)?;
-  thread::sleep(Duration::from_millis(250));
-  fanshim.set_led(255, 0, 0, 0.25)?;
-  thread::sleep(Duration::from_millis(250));
-  fanshim.set_led(0, 0, 0, 1.0)?;
+  let mut h: u16 = 0;
+
+  loop {
+    println!("{}", h);
+    let hsl = Hsl::new(h as f64, 100.0, 50.0, Some(1.0));
+    let rgb = Rgb::from(&hsl);
+
+    // println!("{:?}", hsl);
+    println!(
+      "{} {} {}",
+      rgb.get_red() as u8,
+      rgb.get_green() as u8,
+      rgb.get_blue() as u8
+    );
+    fanshim.set_led(
+      rgb.get_red() as u8,
+      rgb.get_green() as u8,
+      rgb.get_blue() as u8,
+      1.0,
+    )?;
+
+    thread::sleep(Duration::from_millis(100));
+
+    h = (h + 1) % 360;
+  }
+
   Ok(())
 }
